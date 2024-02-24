ada4271b5c6fbf3c31cce887f5b648bf04e9ac69
Leon <leonenko-valera@maul.ru>
2024-02-24 16:11:00 +0300

    task context finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import './App.css';
+import { ThemeProvider } from './components/context/changeThemeContext';
+import { LoginForm } from './components/login-form/login-form';
+import { CustomizedSwitches } from './components/material-ui-toggle/toggle';
 import { SignUpForm } from './components/sign-up-form/sign-up-form';
 
 function App() {
-  return <SignUpForm />;
+  return (
+    <>
+      <CustomizedSwitches />
+      <LoginForm />
+    </>
+  );
 }
 
 export default App;
